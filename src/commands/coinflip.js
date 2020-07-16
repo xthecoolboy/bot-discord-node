22ea7dea2bfaab870d91280de290948519ce0ffc
xthecoolboy <67747753+xthecoolboy@users.noreply.github.com>
2020-07-16 21:53:03 +0200

    Update and rename botinfo.js to coinflip.js

---
+++ src/commands/coinflip.js
@@ -0,0 +1,6 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+exports.run = (client, message, args) => {
+      var coinflip = ['Heads!','Tails!'];
+      message.channel.send(coinflip[Math.floor(Math.random () * coinflip.length)]);
+}
